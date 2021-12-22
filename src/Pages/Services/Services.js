65d483cd6809aa1f35034058bb18d7d9eff4ffa8
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-12-22 23:47:36 +0600

    Service component added

---
+++ src/Pages/Services/Services.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './Services.css';
+
+const Services = () => {
+  return (
+    <div className="container my-5 pt-5 pb-md-5">
+            <div className="d-flex justify-content-center">
+                <div className="text-center">
+                    <h2 className="google-font text-warning">Enjoy Our</h2>
+                    <h1 className="text-size fw-bold">Quility Services</h1>
+                    <p className="text-muted">We specialise in event photography with the natural reportage documentation of corporate occasions and celebrations, producing the highest quality imagery that showcase your company and event in its best possible light. Whatever and whenever your occasion happens, our multi-talented event photographers will cover your occasion to your exact requirements. </p>
+                </div>
+
+            </div>
+
+            <div className="row g-4 my-3">
+
+                <div className="col-12 col-md-3">
+                    <div className="card text-center border-0">
+                        <img src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/h1-custom-icon-img-1.png" className="w-25 mx-auto img-hover" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title my-2">Restaurants</h5>
+                            <small className="card-text text-muted">Over 10 years of Special Photography for 200+ clients of all types.</small>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-3">
+                    <div className="card text-center border-0">
+                        <img src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/h1-custom-icon-img-2.png" className="w-25 mx-auto img-hover" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title my-2">Sightseeing</h5>
+                            <small className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</small>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-3">
+                    <div className="card text-center border-0">
+                        <img src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/h1-custom-icon-img-3.png" className="w-25 mx-auto img-hover" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title my-2">Shops &amps; Boutiques</h5>
+                            <small className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</small>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-3">
+                    <div className="card text-center border-0">
+                        <img src="https://setsail.qodeinteractive.com/wp-content/uploads/2018/09/h1-custom-icon-img-4.png" className="w-25 mx-auto img-hover" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title my-2">Where to Stay</h5>
+                            <small className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</small>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+  );
+};
+
+export default Services;
---
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+import logo from "../../../images/logo-removebg.png"
+
+
+const Navigation = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar style={{ backgroundColor: "#212121" }} position="static">
+        <Toolbar >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <img width="47px" src={logo} alt="logo" /><span style={{ color: "#ef6c00" }} >DAZ</span><span style={{ color: "#ff9800" }} >ZLE</span>
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navigation;
