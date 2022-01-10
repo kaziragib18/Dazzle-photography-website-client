83be60443cc96f927b23bb2d10082d9080a156b1
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2022-01-11 00:00:16 +0600

    Responsive navbar added with mui drawer

--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -6,58 +6,145 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import logo from "../../../images/logo-removebg.png"
+// import logo from "../../../images/logo-removebg.png"
 import { Link } from 'react-router-dom';
-import useAuth from '../../../hooks/useAuth'
+import useAuth from '../../../hooks/useAuth';
+import { makeStyles } from '@mui/styles';
+import { useTheme } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 const Navigation = () => {
+  const theme = useTheme()
+  const useStyle = makeStyles({
+    navItem: {
+      color: 'lightgray',
+      textDecoration: 'none'
+    },
+    navIcon: {
+      [theme.breakpoints.up('sm')]: {
+        display: "none !important"
+      }
+    },
+
+    navItemContainer: {
+      [theme.breakpoints.down('sm')]: {
+        display: "none !important"
+      }
+    },
+    drawerItem: {
+      textDecoration: "none",
+      color: "black",
+      fontFamily: 'poppins',
+      fontSize: 15,
+      fontWeight: 800,
+    },
+  })
+  const { navItem, navIcon, navItemContainer, drawerItem } = useStyle();
+  const [state, setState] = React.useState(false);
+
   const { user, logOut } = useAuth()
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar style={{ backgroundColor: "#212121" }} position="static">
-        <Toolbar >
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 0 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <img width="47px" src={logo} alt="logo" /><span style={{ color: "#ef6c00" }} >DAZ</span><span style={{ color: "#ff9800" }} >ZLE</span>
-          </Typography>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar style={{ backgroundColor: "#212121" }} position="static">
+          <Toolbar >
+            <IconButton
+              size="large"
+              edge="start"
+              color="warning"
+              aria-label="menu"
+              sx={{ mr: 0 }}
+              className={navIcon}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, }}>
+              DAZZLE
+            </Typography>
 
-          <Link
-            style={{ textDecoration: "none", color: "lightgray" }}
-            to='/home'> <Button color="inherit"
-              sx={{ fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}
-            >Home</Button>
-          </Link>
-
-          <Link style={{ textDecoration: "none", color: "lightgray" }}
-            to='/allPackages'><Button color="inherit"
-              sx={{ fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}>Packages</Button></Link>
-
-          {user?.email ?
-            <Box>
-              <Link style={{ textDecoration: "none", color: "lightgray" }} to='/dashboard'><Button color="inherit"
-                sx={{ fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}>Dashboard</Button>
+            <Box className={navItemContainer}>
+              <Link
+                className={navItem}
+                to='/home'> <Button color="warning"
+                  sx={{ fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}
+                >Home</Button>
               </Link>
 
-              <Button onClick={logOut} color="inherit"
-                sx={{ color: "#b71c1c", fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}>Logout</Button>
-            </Box>
-            :
-            <Link style={{ textDecoration: "none", color: "lightgray" }} to='/login'><Button color="inherit"
-              sx={{ fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}>Login</Button>
-            </Link>
-          }
+              <Link className={navItem}
+                to='/allPackages'><Button color="warning"
+                  sx={{ fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}>Packages</Button></Link>
 
-        </Toolbar>
-      </AppBar>
-    </Box>
+              {user?.email ?
+                <>
+                  <Link className={navItem} to='/dashboard'><Button color="warning"
+                    sx={{ fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}>Dashboard</Button>
+                  </Link>
+
+                  <Button onClick={logOut} color="inherit"
+                    sx={{ color: "#b71c1c", fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}>Logout</Button>
+                </>
+                :
+                <Link className={navItem} to='/login'>
+                  <Button color="warning"
+                    sx={{ fontFamily: "poppins", fontWeight: "500", fontSize: "17px" }}>Login</Button>
+                </Link>
+              }
+            </Box>
+
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div>
+        <React.Fragment>
+          <Drawer
+            open={state}
+            onClose={() => setState(false)}
+          >
+            <Box sx={{ width: 250 }} role="presentation">
+              <List>
+                <ListItem button>
+                  <ListItemText>
+                    <Link className={drawerItem} to="/home">HOME</Link>
+                  </ListItemText>
+                </ListItem>
+                <Divider />
+
+                <ListItem button>
+                  <ListItemText> <Link className={drawerItem} to="/allPackages">ALL PACKAGES</Link></ListItemText>
+                </ListItem>
+                <Divider />
+
+                {user?.email ?
+                  <>
+                    <ListItem button>
+                      <ListItemText>
+                        <Link className={drawerItem} to="/dashboard"><Button style={{ fontFamily: 'poppins', fontSize: 14, fontWeight: 700 }} color="inherit">Dashboard</Button></Link>
+                      </ListItemText>
+                    </ListItem>
+                    <Divider />
+                    <ListItem button>
+                      <ListItemText>
+                        <Link className={drawerItem} to="/login"><Button onClick={logOut} style={{ color: "#b71c1c", fontFamily: 'poppins', fontSize: 16, marginBottom: 2, fontWeight: 700 }} color="error">Logout</Button></Link>
+                      </ListItemText>
+                    </ListItem>
+                  </>
+                  :
+                  <ListItem button>
+                    <ListItemText>
+                      <Link className={drawerItem} to="/login"> <Button style={{ textDecoration: "none", color: "dark", fontFamily: 'poppins', fontSize: 14, fontWeight: 700 }} color="inherit">Login</Button></Link>
+                    </ListItemText>
+                  </ListItem>
+                }
+              </List>
+            </Box>
+          </Drawer>
+        </React.Fragment>
+      </div>
+    </>
   );
 };
 
